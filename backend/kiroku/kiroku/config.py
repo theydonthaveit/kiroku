f997183c609458ea7c6b91599e4884b5ef4e9d3d
Alan Williams <theydonthaveit@gmail.com>
2018-02-25 16:18:19 +0000

    init setup via cookiecutter for Flask API development

---
+++ backend/kiroku/kiroku/config.py
@@ -0,0 +1,20 @@
+import os
+
+
+class Config(object):
+
+    ALEMBIC_CONTEXT = {
+        'render_as_batch': True
+    }
+
+
+class Dev(Config):
+
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:////opt/code/kiroku.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    BASIC_AUTH_USERNAME = 'admin'
+    BASIC_AUTH_PASSWORD = 'secret'
+
+
+settings = globals()[os.environ.get('FLASK_CONFIG', 'Dev')]
---
+++ backend/kiroku/kiroku/wsgi.py
@@ -0,0 +1,4 @@
+from kiroku.app import create_app  # pragma: no cover
+
+
+app = create_app()  # pragma: no cover
